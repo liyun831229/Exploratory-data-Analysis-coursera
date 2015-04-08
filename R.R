ced9e98c426fd54a607fba01f9d72a2bfb41585b
liyun831229 <yli0131@gmail.com>
2015-04-08 12:18:31 -0400

    Update R.R

--- R.R
+++ R.R
@@ -4,11 +4,55 @@ dat$Date <- as.Date(dat$Date, "%d/%m/%Y")
 idx <- which(dat$Date == "2007-02-02"| dat$Date == "2007-02-01" )
 keep <- dat
 dat <- dat[idx,]
-dat$Time <- strptime(paste(dat$Date,dat$Time[1:4],sep =","),"%Y-%m-%d, %H:%M:%s")
-
+dat$Time <- strptime(paste(dat$Date,dat$Time,sep =","),"%Y-%m-%d,%H:%M:%S")
+lapply(dat,function(x){grep("\\?",x)})
+#no missing data
+dat[,3] <- as.numeric(dat[,3])
+dat[,4] <- as.numeric(dat[,4])
+dat[,5] <- as.numeric(dat[,5])
+dat[,6] <- as.numeric(dat[,6])
+dat[,7] <- as.numeric(dat[,7])
+dat[,8] <- as.numeric(dat[,8])
+dat[,9] <- as.numeric(dat[,9])
+save.image('dat',file = 'dat.RData')
 png("plot1.png")
 hist(as.numeric(dat[,3]),xlab = 'Global Active Power(kilowatts)', main = "Global Active Power", col = 'red')
 dev.off()
 
-with(dat, plot(dat[,2],as.numeric(dat[,3])))
+
+load('dat.RData')
+Sys.setlocale("LC_TIME", "English")
+png("plot2.png")
+with(dat, plot(Time, Global_active_power,type = 'l',ylab = "Global active power (kilowatts)",xlab = ''))
+dev.off()
+
+load('dat.RData')
+Sys.setlocale("LC_TIME", "English")
+png("plot3.png")
+with(dat, {
+  plot(Time, Sub_metering_1, yaxt = 'n', type = 'l',xlab = '',ylab = 'Energy sub metering')
+  axis(side=2, at=seq(0, 40, by=10))
+  lines(Time,Sub_metering_2,col= 'red')
+  lines(Time, Sub_metering_3, col = 'blue')
+  legend('topright', legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col = c('black','red','blue'),lty = 1)
+})
+dev.off()
+
+load('dat.RData')
+Sys.setlocale("LC_TIME", "English")
+png('plot.png')
+with(dat, {
+    par(mfrow = c(2, 2))
+    plot(Time, Global_active_power,ylab = 'Global active power',type = 'l',xlab = '')
+    plot(Time, Voltage,ylab = 'Voltage',xlab = 'datetime',type = 'l')
+    plot(Time, Sub_metering_1, yaxt = 'n', type = 'l',xlab = '',ylab = 'Energy sub metering')
+       axis(side=2, at=seq(0, 40, by=10))
+       lines(Time,Sub_metering_2,col= 'red')
+       lines(Time, Sub_metering_3, col = 'blue')
+       legend('topright', legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col = c('black','red','blue'),lty = 1)
+
+    plot(Time, Global_reactive_power, ylab = 'Global_reactive_power', xlab = 'datetime',type = 'l')
+})
+dev.off()
+
 
